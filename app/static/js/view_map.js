fcad5500903bcf69df093aaaaaaf388761662c99
Ajay Ramesh <ajayramesh.dev@gmail.com>
2015-02-08 13:54:15 -0800

    finished Reactifying Dashboard

--- app/static/js/view_map.js
+++ app/static/js/view_map.js
@@ -3,8 +3,8 @@ var CSViewMap  = React.createClass({displayName: "CSViewMap",
         return {coords:[]}
     },
     componentDidMount: function(){
-        var geoOptions = { maximumAge: 30000,  //  Valid for 5 minutes
-            timeout:5000,  // Wait 5 seconds
+        var geoOptions = { maximumAge: 30000,
+            timeout:5000,
             enableHighAccuracy:true
         }
         if(navigator.geolocation) {
@@ -14,7 +14,7 @@ var CSViewMap  = React.createClass({displayName: "CSViewMap",
         } else {
             console.warn('Geolocation isnt available for this user.');
         }
-        /* Not quite sure what this does, but it seems important!
+        /* What does this do ?
         setTimeout(function () {
             if(!coords){
                 window.console.log("No confirmation from user, using fallback");
@@ -34,15 +34,32 @@ var CSViewMap  = React.createClass({displayName: "CSViewMap",
         }else{
             csMap = React.createElement(CSMap, {coords: this.state.coords, zoom: 12})
         }
-        return csMap;
+        return (
+            React.createElement("div", null, 
+                React.createElement(CSSearchBox, null), 
+                csMap
+            )
+        )
+    }
+});
+
+var CSSearchBox = React.createClass({displayName: "CSSearchBox",
+    render: function(){
+        return (
+            React.createElement("input", {id: "pac-input", className: "controls", type: "text", placeholder: "Search for a location here."})
+        )
     }
 });
 
 var CSMap = React.createClass({displayName: "CSMap",
+
     getInitialState: function() {
-        return {markers:[],map:null}
+        return {map:null, mapData:null}
     },
     getDefaultProps: function(){
+        /* Coords of the United States. Default if this component is rendered without props 
+           They will be overriden with whatever the user's coordinates are 
+        */
         return {coords:[37.6,-95.665],zoom:5}
     },
     componentDidMount: function(){
@@ -53,11 +70,29 @@ var CSMap = React.createClass({displayName: "CSMap",
         };
         var map = new google.maps.Map(this.getDOMNode(),mapOptions);
         this.setState({map:map});
-
-        var infoWindow = new google.maps.InfoWindow();
-        $.get('/api/v1/hazard/?format=json', function(data){
-            console.log(data);
-        });
+        if(this.state.map){
+            $.get('/api/v1/hazard/?format=json', function(mapData){
+                if(mapData) {
+                    this.setState({mapData:mapData});
+                    this.drawMarkers();
+                }
+            }.bind(this));
+        }
+        /*
+            Another example of a component that could have been easily "Reactified" but can't because Google...
+        */
+        var input = document.getElementById('pac-input');
+        this.state.map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        var searchBox = new google.maps.places.SearchBox(input);
+        // Listen for the event fired when the user selects an item from the
+        // pick list. Retrieve the matching places for that item.
+        google.maps.event.addListener(searchBox, 'places_changed', function () { 
+            var places = searchBox.getPlaces();
+            if (places.length == 0) {
+                return;
+            }
+            this.drawPlaces(places);
+        }.bind(this));
     },
     componentDidUpdate: function(){
         if(this.state.map){
@@ -67,6 +102,67 @@ var CSMap = React.createClass({displayName: "CSMap",
     },
     centerMap: function(){
         return new google.maps.LatLng(this.props.coords[0], this.props.coords[1]);
+    },
+    drawMarkers: function(){
+        var infoWindow = new google.maps.InfoWindow();
+        for (var i = 0; i < this.state.mapData.objects.length; i++) {
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(this.state.mapData.objects[i].lat, this.state.mapData.objects[i].lon),
+                map: this.state.map,
+                animation: google.maps.Animation.DROP,
+                title: this.state.mapData.objects[i].description,
+            });
+            /* Unfortunately the Maps API requires an HTML string. Can't "Reactify". 
+               This would have been the perfect example of a reusable web component.
+            */
+            contentString = '<div class="infoindow">' +
+                '<h4><span class="blue">User: </span>' + this.state.mapData.objects[i].user_type + '</h4>' +
+                '<p> <span class="blue">Date and Time: </span>' + this.state.mapData.objects[i].date_time + '<br>' +
+                '<span class="blue">Hazard: </span>' + this.state.mapData.objects[i].hazard_type + '<br>' +
+                '<span class="blue">Description: </span>' + this.state.mapData.objects[i].description + '</p>' +
+                '</div>';
+            google.maps.event.addListener(marker, 'mouseover', (function (marker, contentString) {
+                return function () {
+                    infoWindow.setContent(contentString);
+                    infoWindow.open(this.state.map, marker);
+                }.bind(this);
+            }.bind(this))(marker, contentString));
+        }
+    },
+    drawPlaces: function(places){
+        /* What does this do ? */
+        /*for (var i = 0, marker; marker = markers[i]; i++) {
+                marker.setMap(null);
+        }
+
+        // For each place, get the icon, place name, and location.
+        markers = [];*/
+        var bounds = new google.maps.LatLngBounds();
+        for (var i = 0, place; place = places[i]; i++) {
+            var image = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            var marker = new google.maps.Marker({
+                map: this.state.map,
+                icon: image,
+                title: place.name,
+                position: place.geometry.location,
+            });
+
+            //Center the map on the new marker's location.
+            this.state.map.panTo(place.geometry.location);
+            /* What does this do ? */
+            //Add marker to map.
+            //markers.push(marker);
+            return true;
+
+        }
     },
     render: function(){
         return (
@@ -81,119 +177,3 @@ $(document).ready(function(){
         $('.content')[0]
     );
 });
-
-/*
-//Generate the map and event listeners using lat and lon.
-function mapGenerator(lat, lon) {
-    var markers = [];
-    var mapOptions = {
-        center: new google.maps.LatLng(lat, lon),
-        zoom: 12,
-        mapTypeId: google.maps.MapTypeId.ROAD
-    };
-
-    //Create the map at the specified element.
-    var map = new google.maps.Map(
-        document.getElementById("map-canvas"),
-        mapOptions);
-
-
-    //Generate markers and search box.
-    markerGenerator(map);
-    return searchboxGenerator(map, markers);
-}
-
-//Generate map markers, info windows, and event listeners.
-function markerGenerator(map) {
-    //Get current data for map.
-    var contentString;
-    var infoWindow = new google.maps.InfoWindow();
-    var mapData = httpGet('/api/v1/hazard/?format=json');
-
-    //Add markers to map.
-    for (var i = 0; i < mapData.objects.length; i++) {
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(mapData.objects[i].lat, mapData.objects[i].lon),
-            map: map,
-            animation: google.maps.Animation.DROP,
-            title: mapData.objects[i].description,
-
-        });
-        contentString = '<div class="infoindow">' +
-            '<h4><span class="blue">User: </span>' + mapData.objects[i].user_type + '</h4>' +
-            '<p> <span class="blue">Date and Time: </span>' + mapData.objects[i].date_time + '<br>' +
-            '<span class="blue">Hazard: </span>' + mapData.objects[i].hazard_type + '<br>' +
-            '<span class="blue">Description: </span>' + mapData.objects[i].description + '</p>' +
-            '</div>';
-
-        google.maps.event.addListener(marker, 'mouseover', (function (marker, contentString) {
-            return function () {
-                infoWindow.setContent(contentString);
-                infoWindow.open(map, marker);
-            }
-        })(marker, contentString));
-    }
-
-    return marker;
-}
-
-//Get data from API to generate markers.
-function httpGet(requestUrl) {
-
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", requestUrl, false);
-    xmlHttp.send(null);
-
-    return JSON.parse(xmlHttp.responseText);
-}
-
-//Add searchbox to map. When place is selected, add markers and lat and lon to form.
-function searchboxGenerator(map, markers) {
-// Create the search box and link it to the UI element.
-    var input = document.getElementById('pac-input');
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-    var searchBox = new google.maps.places.SearchBox(input);
-
-    // Listen for the event fired when the user selects an item from the
-    // pick list. Retrieve the matching places for that item.
-    google.maps.event.addListener(searchBox, 'places_changed', function () {
-        var places = searchBox.getPlaces();
-
-        if (places.length == 0) {
-            return;
-        }
-        for (var i = 0, marker; marker = markers[i]; i++) {
-            marker.setMap(null);
-        }
-
-        // For each place, get the icon, place name, and location.
-        markers = [];
-        var bounds = new google.maps.LatLngBounds();
-        for (var i = 0, place; place = places[i]; i++) {
-            var image = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25)
-            };
-
-            // Create a marker for each place.
-            var marker = new google.maps.Marker({
-                map: map,
-                icon: image,
-                title: place.name,
-                position: place.geometry.location,
-            });
-
-            //Center the map on the new marker's location.
-            map.panTo(place.geometry.location);
-            //Add marker to map.
-            markers.push(marker);
-
-            return true;
-
-        }
-    });
-
-}*/
